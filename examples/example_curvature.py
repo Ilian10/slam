03df7d2b3ce7172e271f26b5606ba9256562cf1b
Ilian10 <salmi.ilian@gmail.com>
2026-02-05 13:41:04 +0100

    Comment out curvature estimation and plotting code
    Comment out sections related to curvature calculations and visualizations.

--- examples/example_curvature.py
+++ examples/example_curvature.py
@@ -30,121 +30,121 @@ import slam.curvature as scurv
 mesh_file = "/envau/work/meca/users/salmi.i/Surf_Hemi_full/sub-CC00050XX01_ses-7201_desc-restore_T2w_nnUNet_split.nii.gz/sub-CC00050XX01_ses-7201_desc-restore_T2w_nnUNet_split_extract_surf_right_hemi_n15.gii"
 mesh = sio.load_mesh(mesh_file)
 
-###############################################################################
-# Comptue estimations of principal curvatures
-PrincipalCurvatures, PrincipalDir1, PrincipalDir2 \
-    = scurv.curvatures_and_derivatives(mesh)
+# ###############################################################################
+# # Comptue estimations of principal curvatures
+# PrincipalCurvatures, PrincipalDir1, PrincipalDir2 \
+#     = scurv.curvatures_and_derivatives(mesh)
 
-###############################################################################
-# Comptue Gauss curvature from principal curvatures
-gaussian_curv = PrincipalCurvatures[0, :] * PrincipalCurvatures[1, :]
+# ###############################################################################
+# # Comptue Gauss curvature from principal curvatures
+# gaussian_curv = PrincipalCurvatures[0, :] * PrincipalCurvatures[1, :]
 
-###############################################################################
-# Comptue mean curvature from principal curvatures
-mean_curv = 0.5 * (PrincipalCurvatures[0, :] + PrincipalCurvatures[1, :])
+# ###############################################################################
+# # Comptue mean curvature from principal curvatures
+# mean_curv = 0.5 * (PrincipalCurvatures[0, :] + PrincipalCurvatures[1, :])
 
-###############################################################################
-# Decomposition of the curvatures into ShapeIndex and Curvedness
-# Based on 'Surface shape and curvature scales
-#           Jan JKoenderink & Andrea Jvan Doorn'
-shapeIndex, curvedness = scurv.decompose_curvature(PrincipalCurvatures)
+# ###############################################################################
+# # Decomposition of the curvatures into ShapeIndex and Curvedness
+# # Based on 'Surface shape and curvature scales
+# #           Jan JKoenderink & Andrea Jvan Doorn'
+# shapeIndex, curvedness = scurv.decompose_curvature(PrincipalCurvatures)
 
-###############################################################################
-# Estimation error on the principal curvature length
-K = [1, 0]
+# ###############################################################################
+# # Estimation error on the principal curvature length
+# K = [1, 0]
 
-quadric = sgps.generate_quadric(
-    K,
-    nstep=[20, 20],
-    ax=3,
-    ay=3,
-    random_sampling=False,
-    ratio=0.3,
-    random_distribution_type="gamma",
-    equilateral=True,
-)
+# quadric = sgps.generate_quadric(
+#     K,
+#     nstep=[20, 20],
+#     ax=3,
+#     ay=3,
+#     random_sampling=False,
+#     ratio=0.3,
+#     random_distribution_type="gamma",
+#     equilateral=True,
+# )
 
-###############################################################################
-# Estimated computation of the Principal curvature, K_gauss, K_mean
-p_curv, d1_estim, d2_estim = scurv.curvatures_and_derivatives(quadric)
+# ###############################################################################
+# # Estimated computation of the Principal curvature, K_gauss, K_mean
+# p_curv, d1_estim, d2_estim = scurv.curvatures_and_derivatives(quadric)
 
-k1_estim, k2_estim = p_curv[0, :], p_curv[1, :]
+# k1_estim, k2_estim = p_curv[0, :], p_curv[1, :]
 
-k_gauss_estim = k1_estim * k2_estim
+# k_gauss_estim = k1_estim * k2_estim
 
-k_mean_estim = 0.5 * (k1_estim + k2_estim)
+# k_mean_estim = 0.5 * (k1_estim + k2_estim)
 
-###############################################################################
-# Analytical computation of the curvatures
+# ###############################################################################
+# # Analytical computation of the curvatures
 
-k_mean_analytic = sgps.quadric_curv_mean(K)(
-    np.array(quadric.vertices[:, 0]), np.array(quadric.vertices[:, 1])
-)
+# k_mean_analytic = sgps.quadric_curv_mean(K)(
+#     np.array(quadric.vertices[:, 0]), np.array(quadric.vertices[:, 1])
+# )
 
-k_gauss_analytic = sgps.quadric_curv_gauss(K)(
-    np.array(quadric.vertices[:, 0]), np.array(quadric.vertices[:, 1])
-)
+# k_gauss_analytic = sgps.quadric_curv_gauss(K)(
+#     np.array(quadric.vertices[:, 0]), np.array(quadric.vertices[:, 1])
+# )
 
-k1_analytic = np.zeros((len(k_mean_analytic)))
-k2_analytic = np.zeros((len(k_mean_analytic)))
+# k1_analytic = np.zeros((len(k_mean_analytic)))
+# k2_analytic = np.zeros((len(k_mean_analytic)))
 
-for i in range(len(k_mean_analytic)):
-    a, b = np.roots((1, -2 * k_mean_analytic[i], k_gauss_analytic[i]))
-    k1_analytic[i] = min(a, b)
-    k2_analytic[i] = max(a, b)
+# for i in range(len(k_mean_analytic)):
+#     a, b = np.roots((1, -2 * k_mean_analytic[i], k_gauss_analytic[i]))
+#     k1_analytic[i] = min(a, b)
+#     k2_analytic[i] = max(a, b)
 
 
-###############################################################################
-# Error computation
+# ###############################################################################
+# # Error computation
 
-k_mean_relative_change = abs(
-    (k_mean_analytic - k_mean_estim) / k_mean_analytic)
-k_mean_absolute_change = abs((k_mean_analytic - k_mean_estim))
+# k_mean_relative_change = abs(
+#     (k_mean_analytic - k_mean_estim) / k_mean_analytic)
+# k_mean_absolute_change = abs((k_mean_analytic - k_mean_estim))
 
-k1_relative_change = abs((k1_analytic - k1_estim) / k1_analytic)
-k1_absolute_change = abs((k1_analytic - k1_estim))
+# k1_relative_change = abs((k1_analytic - k1_estim) / k1_analytic)
+# k1_absolute_change = abs((k1_analytic - k1_estim))
 
-###############################################################################
-# Estimation error on the curvature directions
-# commented because there is a bug:
-# ValueError: shapes (3,2) and (3,2) not aligned: 2 (dim 1) != 3 (dim 0)
-# actually, vec1.shape=(3,) while vec2.shape=(3,2)
+# ###############################################################################
+# # Estimation error on the curvature directions
+# # commented because there is a bug:
+# # ValueError: shapes (3,2) and (3,2) not aligned: 2 (dim 1) != 3 (dim 0)
+# # actually, vec1.shape=(3,) while vec2.shape=(3,2)
 
-K = [1, 0]
+# K = [1, 0]
 
-quadric = sgps.generate_quadric(
-    K,
-    nstep=[20, 20],
-    ax=3,
-    ay=3,
-    random_sampling=False,
-    ratio=0.3,
-    random_distribution_type="gamma",
-    equilateral=True,
-)
+# quadric = sgps.generate_quadric(
+#     K,
+#     nstep=[20, 20],
+#     ax=3,
+#     ay=3,
+#     random_sampling=False,
+#     ratio=0.3,
+#     random_distribution_type="gamma",
+#     equilateral=True,
+# )
 
-###############################################################################
-# Estimated computation of the Principal curvature, Direction1, Direction2
-p_curv_estim, d1_estim, d2_estim = scurv.curvatures_and_derivatives(quadric)
+# ###############################################################################
+# # Estimated computation of the Principal curvature, Direction1, Direction2
+# p_curv_estim, d1_estim, d2_estim = scurv.curvatures_and_derivatives(quadric)
 
-###############################################################################
-# Analytical computation of the directions
-analytical_directions = sgps.compute_all_principal_directions_3D(
-    K, quadric.vertices)
+# ###############################################################################
+# # Analytical computation of the directions
+# analytical_directions = sgps.compute_all_principal_directions_3D(
+#     K, quadric.vertices)
 
-estimated_directions = np.zeros(analytical_directions.shape)
-estimated_directions[:, :, 0] = d1_estim
-estimated_directions[:, :, 1] = d2_estim
+# estimated_directions = np.zeros(analytical_directions.shape)
+# estimated_directions[:, :, 0] = d1_estim
+# estimated_directions[:, :, 1] = d2_estim
 
-angular_error_0, dotprods = ut.compare_analytic_estimated_directions(
-    analytical_directions[:, :, 0], estimated_directions[:, :, 0]
-)
-angular_error_0 = 180 * angular_error_0 / np.pi
+# angular_error_0, dotprods = ut.compare_analytic_estimated_directions(
+#     analytical_directions[:, :, 0], estimated_directions[:, :, 0]
+# )
+# angular_error_0 = 180 * angular_error_0 / np.pi
 
-angular_error_1, dotprods = ut.compare_analytic_estimated_directions(
-    analytical_directions[:, :, 1], estimated_directions[:, :, 1]
-)
-angular_error_1 = 180 * angular_error_1 / np.pi
+# angular_error_1, dotprods = ut.compare_analytic_estimated_directions(
+#     analytical_directions[:, :, 1], estimated_directions[:, :, 1]
+# )
+# angular_error_1 = 180 * angular_error_1 / np.pi
 
 
 #############################################################################
@@ -169,85 +169,88 @@ rot_z = np.array([[np.cos(theta), -np.sin(theta), 0],
 vertices_translate = np.dot(rot_z, vertices_translate.T).T
 
 # Plot Mean Curvature
-display_settings = {}
-display_settings['colorbar_label'] = 'Mean Curvature'
-mesh_data = {}
-mesh_data['vertices'] = vertices_translate
-mesh_data['faces'] = mesh.faces
-mesh_data['title'] = 'example_mesh.gii Mean Curvature'
-intensity_data = {}
-intensity_data['values'] = mean_curv
-intensity_data["mode"] = "vertex"
-Fig = splt.mesh_projection(
-    mesh_data=mesh_data,
-    intensity_data=intensity_data,
-    display_settings=display_settings)
-# Fig.show()
-#Fig.write_html("example_curvature_1.html")
+# display_settings = {}
+# display_settings['colorbar_label'] = 'Mean Curvature'
+# mesh_data = {}
+# mesh_data['vertices'] = vertices_translate
+# mesh_data['faces'] = mesh.faces
+# mesh_data['title'] = 'example_mesh.gii Mean Curvature'
+# intensity_data = {}
+# intensity_data['values'] = mean_curv
+# intensity_data["mode"] = "vertex"
+# Fig = splt.mesh_projection(
+#     mesh_data=mesh_data,
+#     intensity_data=intensity_data,
+#     display_settings=display_settings)
+# # Fig.show()
+# #Fig.write_html("example_curvature_1.html")
 
-# Plot Gaussian Curvature
-mesh_data['title'] = 'example_mesh.gii Gaussian Curvature'
-intensity_data['values'] = gaussian_curv
-display_settings['colorbar_label'] = 'Gaussian Curvature'
-Fig = splt.mesh_projection(
-    mesh_data=mesh_data,
-    intensity_data=intensity_data,
-    display_settings=display_settings)
+# # Plot Gaussian Curvature
+# mesh_data['title'] = 'example_mesh.gii Gaussian Curvature'
+# intensity_data['values'] = gaussian_curv
+# display_settings['colorbar_label'] = 'Gaussian Curvature'
+# Fig = splt.mesh_projection(
+#     mesh_data=mesh_data,
+#     intensity_data=intensity_data,
+#     display_settings=display_settings)
 # Fig.show()
 #Fig.write_html("example_curvature_2.html")
 
-# Plot Shape Index
-mesh_data['title'] = 'example_mesh.gii Shape Index'
-#intensity_data=None
-#display_settings=None
+mesh_data = {}
+mesh_data['vertices'] = vertices_translate
+mesh_data['faces'] = mesh.faces
+
+mesh_data['title'] = 'example_mesh.gii'
+intensity_data=None
+display_settings= {}
 Fig = splt.mesh_projection(
-    mesh_data=mesh_data)#,
-    #intensity_data=intensity_data,
-    #display_settings=display_settings)
+    mesh_data=mesh_data,#,
+    intensity_data=intensity_data,
+    display_settings=display_settings)
 # Fig.show()
 Fig.write_html("example_curvature_3.html")
 
 # Plot Curvedness
-mesh_data['title'] = 'example_mesh.gii Curvedness'
-intensity_data['values'] = curvedness
-display_settings['colorbar_label'] = 'Curvedness'
-Fig = splt.mesh_projection(
-    mesh_data=mesh_data,
-    intensity_data=intensity_data,
-    display_settings=display_settings)
-# Fig.show()
-#Fig.write_html("example_curvature_4.html")
+# mesh_data['title'] = 'example_mesh.gii Curvedness'
+# intensity_data['values'] = curvedness
+# display_settings['colorbar_label'] = 'Curvedness'
+# Fig = splt.mesh_projection(
+#     mesh_data=mesh_data,
+#     intensity_data=intensity_data,
+#     display_settings=display_settings)
+# # Fig.show()
+# #Fig.write_html("example_curvature_4.html")
 
-# Plot Quadric K Mean Absolute Change
-mesh_data['vertices'] = quadric.vertices
-mesh_data['faces'] = quadric.faces
-mesh_data['title'] = 'Quadric K Mean Absolute Change'
-intensity_data['values'] = k_mean_absolute_change
-display_settings['colorbar_label'] = 'K Mean Absolute Change'
-Fig = splt.mesh_projection(
-    mesh_data=mesh_data,
-    intensity_data=intensity_data,
-    display_settings=display_settings)
-# Fig.show()
-#Fig.write_html("example_curvature_5.html")
+# # Plot Quadric K Mean Absolute Change
+# mesh_data['vertices'] = quadric.vertices
+# mesh_data['faces'] = quadric.faces
+# mesh_data['title'] = 'Quadric K Mean Absolute Change'
+# intensity_data['values'] = k_mean_absolute_change
+# display_settings['colorbar_label'] = 'K Mean Absolute Change'
+# Fig = splt.mesh_projection(
+#     mesh_data=mesh_data,
+#     intensity_data=intensity_data,
+#     display_settings=display_settings)
+# # Fig.show()
+# #Fig.write_html("example_curvature_5.html")
 
-# Plot Quadric Angular Error 0
-mesh_data['title'] = 'Quadric Angular Error 0'
-intensity_data['values'] = angular_error_0
-display_settings['colorbar_label'] = 'Angular Error 0'
-Fig = splt.mesh_projection(
-    mesh_data=mesh_data,
-    intensity_data=intensity_data,
-    display_settings=display_settings)
-#Fig.write_html("example_curvature_6.html")
+# # Plot Quadric Angular Error 0
+# mesh_data['title'] = 'Quadric Angular Error 0'
+# intensity_data['values'] = angular_error_0
+# display_settings['colorbar_label'] = 'Angular Error 0'
+# Fig = splt.mesh_projection(
+#     mesh_data=mesh_data,
+#     intensity_data=intensity_data,
+#     display_settings=display_settings)
+# #Fig.write_html("example_curvature_6.html")
 
-# Plot Quadric Angular Error 1
-mesh_data['title'] = 'Quadric Angular Error 1'
-intensity_data['values'] = angular_error_1
-display_settings['colorbar_label'] = 'Angular Error 1'
-Fig = splt.mesh_projection(
-    mesh_data=mesh_data,
-    intensity_data=intensity_data,
-    display_settings=display_settings)
+# # Plot Quadric Angular Error 1
+# mesh_data['title'] = 'Quadric Angular Error 1'
+# intensity_data['values'] = angular_error_1
+# display_settings['colorbar_label'] = 'Angular Error 1'
+# Fig = splt.mesh_projection(
+#     mesh_data=mesh_data,
+#     intensity_data=intensity_data,
+#     display_settings=display_settings)
 # Fig.show()
 #Fig.write_html("example_curvature_7.html")
